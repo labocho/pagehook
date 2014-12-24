fe9c6fcfa2617389842333ed56f88b2398f0bc79
labocho <labocho@penguinlab.jp>
2014-12-24 16:55:06 +0900

    rewrite with es5

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,9 @@
 var gulp = require("gulp");
 var browserify = require("browserify");
-var coffeeify = require("coffeeify");
 var source = require("vinyl-source-stream");
 
 gulp.task("browserify", function() {
-  return browserify({entries: ["./src/pagehook.coffee"], standalone: "Pagehook", debug: true})
-    .transform(coffeeify)
+  return browserify({entries: ["./src/pagehook.js"], standalone: "Pagehook"})
     .bundle()
     .pipe(source("pagehook.js"))
     .pipe(gulp.dest("./lib"));
